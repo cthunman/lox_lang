2be056408d1288dbafcc1278a406a6dc16cacb5b
Carl Thunman <thunman@gmail.com>
2023-01-02 19:47:40 -0500

    project compiles now

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -7,9 +7,10 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1 ) {
             System.out.println("Usage: jlox [script]");
@@ -32,12 +33,14 @@ public class Lox {
                 break;
             }
             run(line);
+            hadError = false;
         }
     }
 
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
     }
 
     private static void run(String source) {
@@ -47,5 +50,14 @@ public class Lox {
         for (Token token: tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
